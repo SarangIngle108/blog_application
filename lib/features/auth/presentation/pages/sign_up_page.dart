ae3dcdda2fea4424bd821c8d4afd81d5da56ddac
Sarang S. Ingle <you@example.com>
2024-03-09 19:24:13 +0530

    completed sign up page ui

--- lib/features/auth/presentation/pages/sign_up_page.dart
+++ lib/features/auth/presentation/pages/sign_up_page.dart
@@ -12,12 +12,26 @@ class SignUpPage extends StatefulWidget {
 }
 
 class _SignUpPageState extends State<SignUpPage> {
+  final formKey = GlobalKey<FormState>();
+  final nameController = TextEditingController();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    nameController.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 15.w),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 15.w),
+        child: Form(
+          key: formKey,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
@@ -29,25 +43,32 @@ class _SignUpPageState extends State<SignUpPage> {
               SizedBox(
                 height: 30.h,
               ),
-              const AuthField(hintText: 'Name'),
+              AuthField(hintText: 'Name', controller: nameController),
               SizedBox(
                 height: 15.h,
               ),
-              AuthField(hintText: 'Email'),
+              AuthField(
+                hintText: 'Email',
+                controller: emailController,
+              ),
               SizedBox(
                 height: 15.h,
               ),
-              AuthField(hintText: 'Password'),
+              AuthField(
+                hintText: 'Password',
+                controller: passwordController,
+                obscureText: true,
+              ),
               SizedBox(
                 height: 20.h,
               ),
-              AuthGradientButton(),
+              const AuthGradientButton(),
               SizedBox(
                 height: 20.h,
               ),
               RichText(
                   text: TextSpan(
-                      text: 'Don\'t have an account? ',
+                      text: 'Already have an account? ',
                       style: Theme.of(context).textTheme.titleMedium,
                       children: [
                     TextSpan(
